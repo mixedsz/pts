8a15135595e80fcebfc8d67819f9f5e2db21ad96
mixedsz <treygoodlik@gmail.com>
2026-05-13 18:06:03 -0400

    Add files via upload

---
+++ simple_pt/fxmanifest.lua
@@ -0,0 +1,34 @@
+shared_script "@ReaperV4/imports/bypass.js"
+shared_script "@ReaperV4/imports/bypass.lua"
+shared_script "@ReaperV4/imports/bypass_s.lua"
+shared_script "@ReaperV4/imports/bypass_c.lua"
+lua54 "yes" -- needed for Reaper
+
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Floh'
+description 'Simple "peacetime" script for RP servers which disables firing and shows status.'
+version '1.0.1'
+
+ui_page "ui/ui.html"
+
+files {
+	"ui/ui.html",
+	"ui/ui.js",
+	"ui/ui.css",
+}
+
+client_scripts {
+	'client.lua',
+}
+
+server_scripts {
+	'server.lua',
+	--[[server.lua]]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            'client/lib/.validate.js',
+}
+
+dependencies {
+    '/onesync',  -- requires state awareness to be enabled
+}
